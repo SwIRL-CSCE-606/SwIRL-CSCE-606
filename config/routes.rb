daa8b4f94e058f0c5342d596db4f97e81e8161fa
Chong Wen <chongwen107@gmail.com>
2023-09-10 01:39:56 -0500

    update the style of routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "static_pages/home"
-  root :to => redirect('/static_pages/home')
+  root 'static_pages#home'
+  # root :to => redirect('/static_pages/home')
 end
