e4352846f4adb3adfcceac2a91ab7bb0e5445fa0
mohitsarin-tamu <mohitsarin26@tamu.edu>
2023-09-26 17:42:50 -0500

    Update seeds.rb
    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Seed the RottenPotatoes DB with some singular_event.
+singular_events = [
+  {:singular_event => 'Movie-Screeing', :name => 'Test',
+    :date => '16-Apr-1988'},
+    {:singular_event => 'Movie-Screeing-2', :name => 'Test-1',
+    :date => '16-Apr-1988'},
+]
+
+singular_events.each do |singular_event|
+  Singular_Event.create!(singular_event)
+end
