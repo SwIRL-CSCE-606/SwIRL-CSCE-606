74f328e723096a0d1b34dcb423e85ab1c1687840
ishant kundra <74143358+ishantkundra@users.noreply.github.com>
2023-09-28 16:43:23 -0500

    ceated main page

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'static_pages#home'
+  root 'static_pages#main'
   # root :to => redirect('/static_pages/home')
+  get 'home' => 'static_pages#home'
   get 'singular' => 'static_pages#singular_event'
   get 'series' => 'static_pages#series_event'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_26_231833) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_26_234524) do
+  create_table "accounts", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "event_informations", force: :cascade do |t|
     t.integer "event_id", null: false
     t.date "date"
