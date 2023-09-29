d145e201bc370f3aa524e07454cd7ac28c91dcc9
mohitsarin-tamu <mohitsarin26@tamu.edu>
2023-09-29 07:19:23 -0500

    added Rspec and Cucumber test cases

--- features/step_definitions/user_story_3.rb
+++ features/step_definitions/user_story_3.rb
@@ -1,52 +1,94 @@
-    #spec/features/user_story_3.rb
-    require 'rails_helper'
+# features/step_definitions/add_people_steps.rb
 
-    RSpec.feature 'Add People to the event' do
-      scenario 'Add People to the event page' do
-        visit root_path
-        click_on 'Organizer Button'
-        expect(page).to have_content('Singular Event')
-        expect(page).to have_content('Series Event')
-        click_on 'Singular Event Button'
-        expect(page).to have_content('Add People')
-        click_on 'Add People'
-        expect(page).to have_content('Email')
-      end
-    
-    
-      scenario 'Add People to the Singular Event' do
-        visit people_list_page_path(event: 'Test Event')
-        fill_in 'Email', with: 'testemail@gmail.com'
-        expect(page).to have_content('Confirmation Message')
-      end
-    
-      scenario 'The details of the person added to the page should be saved in the database' do
-        RSpec.describe User, type: :model do
-            it "creates a new user in the database" do
-        user = User.new(name: "Test", email: "testemail@gmail.com")
-        expect(user.save).to be_truthy
-            end
-        end
-     end
+Given("I am on the Main Page") do
+  visit '/static_pages/main'
+end
 
-    
-      scenario 'User fills out email field on the webpage' do
-        visit people_list_page_path(event: 'Test Event')
-        fill_in 'email', with: 'testemail@gmail.com'
-        click_button 'Submit' 
-        expect(find_field('email').value).to eq('testemail@gmail.com')
-        end
-    end
+When("I click on the Organizer Button") do
+  click_on 'Organizer'
+end
 
+Then("I should be on the Organizer Page") do
+  expect(page).to have_content('Organizer Page')
+end
 
+And("I should see options for Singular Event, Series Event") do
+  expect(page).to have_content('Singular Event')
+  expect(page).to have_content('Series Event')
+end
 
+When("I click on the Singular Event page") do
+  click_on 'Singular Event'
+end
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
+Then("I should see the Singular Event page") do
+  expect(page).to have_content('Singular Event Page')
+end
+
+When("I click on the add people in the singular event") do
+  click_on 'Add People'
+end
+
+Then("I should see the add people to the singular event page where I can add the email of the persons to be added to a particular singular event") do
+  expect(page).to have_content('Add People to Singular Event Page')
+end
+
+Given("I am on the Organizer Page") do
+  visit '/static_pages/home'
+end
+
+When("I click on Singular Event") do
+  click_on 'Singular Event'
+end
+
+Then("I should be on the Singular Event Creation Page") do
+  expect(page).to have_content('Singular Event Creation Page')
+end
+
+When("I click on Add people to the event") do
+  click_on 'Add People to the Event'
+end
+
+Then("I should be on the add people to the event page") do
+  expect(page).to have_content('Add People to the Event Page')
+end
+
+And("I fill in the following details:") do |table|
+  table.hashes.each do |row|
+    fill_in 'Email', with: row['Email']
+  end
+end
+
+And("I click on the Submit Button") do
+  click_on 'Submit'
+end
+
+Then("this email details should be saved in the database") do
+  expect(EventParticipant.find_by(email: 'testemail@gmail.com')).to be_present
+end
+
+Given("I am on the People List Page for the Test Event") do
+  visit '/people_list'
+end
+
+When("I add a person with the following email id:") do |table|
+  table.hashes.each do |row|
+    fill_in 'Email', with: row['Email']
+    click_on 'Add Person'
+  end
+end
+
+Then("it should add this record to the database and reload the page for the new entry") do
+  expect(EventParticipant.find_by(email: 'testemail@gmail.com')).to be_present
+end
+
+When("I again add a person with the email id:") do |table|
+  table.hashes.each do |row|
+    fill_in 'Email', with: row['Email']
+    click_on 'Add Person'
+  end
+end
+
+Then("the email details should be saved in the database") do
+  expect(EventParticipant.find_by(email: 'testemail2@gmail.com')).to be_present
+end
