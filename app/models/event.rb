610f4b76c393a871d3f3991974908d0ccd43c884
Erik Priest <epriest@tamu.edu>
2023-11-10 09:52:52 -0600

    moved all schema changes to migration files and consolidated migration files

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
     has_many :attendee_infos
+    has_many :time_slots
     accepts_nested_attributes_for :attendee_infos
     has_one :event_info 
     has_one_attached :csv_file
