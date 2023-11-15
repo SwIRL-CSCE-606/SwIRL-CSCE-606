4ae0baa24e70e6009ca432c2e139a5bba8d2eaf1
ishant kundra <74143358+ishantkundra@users.noreply.github.com>
2023-11-15 13:34:44 -0600

    v

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_11_190816) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_10_154255) do
   create_table "accounts", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -22,8 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_11_190816) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -42,7 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_11_190816) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
