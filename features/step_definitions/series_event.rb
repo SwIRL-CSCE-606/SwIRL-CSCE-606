5b9881013e2dfa8b2984cd45bb838d719229c1e8
Shaunak Joshi <shaunakjoshi1407@gmail.com>
2023-11-24 17:20:59 -0600

    Add and fix cucumber tests (#162)
    * remove unused tests
    
    * add two big cucumber tests
    
    * remove some test statements
    
    * comment  some lines regarding series event
    
    ---------
    
    Co-authored-by: Chong Wen <chongwen107@gmail.com>

--- features/step_definitions/series_event.rb
+++
@@ -1,7 +0,0 @@
-  When("I click on the Series Event page") do
-    click_link 'Create series event'
-  end
-
-  Then("I should see the Series Event page") do
-    expect(page).to have_text('Series Event Form')
-  end
