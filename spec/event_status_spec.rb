4e8967f75680338d5f1f20d81b9942acf741db13
Erik Priest <epriest@tamu.edu>
2023-11-30 03:05:14 -0600

    fixed incorrect rspec cases and shows proper time now for singular event instead of just start time, now it shows start time - end time

--- spec/event_status_spec.rb
+++ spec/event_status_spec.rb
@@ -8,7 +8,7 @@ RSpec.feature 'EventStatus', type: :feature do
 
     event = Event.create(name: 'Item 6')
     event_info = EventInfo.create(name: 'Item 6', description: 'event 6', date: DateTime.parse('26-May-2023'),
-                                  venue: 'Venue 6', start_time: Time.parse('00:00:00AM').in_time_zone('Central Time (US & Canada)'), event:)
+                                  venue: 'Venue 6', start_time: Time.parse('00:00:00AM').in_time_zone('Central Time (US & Canada)'), end_time: Time.parse('06:00:00AM').in_time_zone('Central Time (US & Canada)'), event:)
     attendee_info = AttendeeInfo.create(name: 's1mple', email: 'example7@gmail.com', is_attending: 'no',
                                         comments: 'N/A', event:)
 
@@ -21,7 +21,7 @@ RSpec.feature 'EventStatus', type: :feature do
 
     # Now check for the content
     expect(page).to have_content('Item 6')
-    expect(page).to have_content('06:00 AM') # matching strftime('%H:%M %p')
+    expect(page).to have_content('06:00 AM - 12:00 PM') # matching strftime('%H:%M %p')
     expect(page).to have_content('Venue 6')
     expect(page).to have_content('s1mple')
     expect(page).to have_content('example7@gmail.com')
